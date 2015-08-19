d8c58115857d8b0c18f48eb578be8ffbd71fffb3
Guillermo Phillips <guillermo.phillips@chromasports.com>
2015-08-19 10:19:43 +0100

    Further tweaks

--- getyoutubechannel.php
+++ getyoutubechannel.php
@@ -1,5 +1,6 @@
 <?php
 	header('Content-Type: text/plain');
+	ini_set('html_errors', false);
 
 	function CURLGet($url) {
 		$curl = curl_init();
@@ -20,7 +21,7 @@
 		$items = $json->items;
 
 
-		$xml = <<<"XML"
+		$xml = <<<XML
 <?xml version='1.0' encoding='UTF-8'?>
 <feed xmlns='http://www.w3.org/2005/Atom'
     xmlns:openSearch='http://a9.com/-/spec/opensearch/1.1/'
@@ -36,11 +37,29 @@ XML;
 
 		foreach ($items as $item) {
 			$snippet = $item->snippet;
-			$title = $snippet->title;
-			$description = $snippet->description;
+			$thumbnail_url = $snippet->thumbnails->default->url;
+			$video_id = $snippet->resourceId->videoId;
+
+			$json_video = json_decode(CURLGet("https://www.googleapis.com/youtube/v3/videos?id=$video_id&part=contentDetails,statistics,suggestions&key=AIzaSyBbQeECnN1g6ez9RRtyuuOxBq5LBGsTbDE"));
+
+			print_r($json_video);
+
+			$duration = $json_video->items[0]->contentDetails->duration;
+			$view_count = $json_video->items[0]->statistics->viewCount;
+			$favourite_count = $json_video->items[0]->statistics->favoriteCount;
+			$author_name = "dunno";
 
 			$xml .= "\t<entry>\r\n";
-			$xml .= "\t\t<title>$title</title>\r\n";
+			$xml .= "\t\t<id>>$video_id</id>\r\n";
+			$xml .= "\t\t<gd:rating min='' max='' numRaters='' average='' />\r\n";
+			$xml .= "\t\t<yt:statistics viewCount='$view_count' favoriteCount='$favourite_count'>\r\n";
+			$xml .= "\t\t<author>\r\n";
+			$xml .= "\t\t\t<name>$author_name</name>\r\n";
+			$xml .= "\t\t</author>\r\n";
+			$xml .= "\t\t<media:group>\r\n";
+			$xml .= "\t\t\t<yt:duration seconds='$duration'>\r\n";
+			$xml .= "\t\t\t<media:thumbnail url='$thumbnail_url' />\r\n";
+			$xml .= "\t\t</media:group>\r\n";
 			$xml .= "\t</entry>\r\n";
 		}
 
@@ -55,8 +74,8 @@ XML;
 
 	$json = json_decode(CURLGet("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=$uploads_playlist_id&key=AIzaSyBbQeECnN1g6ez9RRtyuuOxBq5LBGsTbDE"));
 
-	echo MapV2toV3($json);
-	echo '-----------------------------------------------------------------------------';
-	print_r($json);
+	print_r(MapV2toV3($json));
+	echo "-----------------------------------------------------------------------------\r\n";
+	//print_r($json);
 
 ?>
